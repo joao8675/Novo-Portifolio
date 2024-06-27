65f5a1d6078fb516f4d1a66cea5264b55923466e
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-06-27 00:28:01 -0300

    finish

--- JS/showDatails.js
+++ JS/showDatails.js
@@ -2,37 +2,37 @@ function showDetails(detailsId) {
     const details = {
         details1: {
             title: 'Delicacy Pizzaria',
-            info: 'Este é um site de restaurante de pizza para auxiliar no processo das entregas. nele e possivel ver as promocoes, realizar pedidos, realizar o pagamento online e conversar com o atendente se necessario.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla praesentium quae, optio expedita sunt consequatur vitae accusamus perspiciatis explicabo et reprehenderit ipsa nemo modi natus eos consectetur debitis officia? Ex.',
+            info: 'Tem o objetivo de facilitar o processo de entregas e garantir que você tenha a melhor experiência possível com nossas deliciosas pizzas. No nosso site, você pode acompanhar promoções exclusivas que oferecemos regularmente. Desde descontos em pizzas tradicionais até combos especiais com bebidas e sobremesas, você sempre encontrará algo para satisfazer seu apetite.Além disso, você pode navegar pelo nosso menu diversificado, que inclui uma ampla variedade de pizzas com diferentes sabores e ingredientes. Escolha suas preferidas e monte seu pedido de forma rápida e fácil.',
             skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub'],
             images: ['../Styles/Assets/Delicacy.png', '../Styles/Assets/Delicacy.png'], // URLs das imagens do projeto
             link: 'https://joao8675.github.io/Delicacy-site-/'
         },
         details2: {
             title: 'Dashboard Invester',
-            info: 'Este é um painel de controle para um banco.Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsa repellendus voluptates. Odio odit praesentium similique officiis molestiae veniam laboriosam quidem nesciunt, et dolorem ea, aperiam, maiores saepe vitae fuga.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus impedit dolorem amet, fuga officia, numquam exercitationem dignissimos quibusdam sit consequuntur facilis porro eligendi voluptatibus quo provident, laborum deleniti velit incidunt!',
+            info: 'O Inverster é uma plataforma intuitiva e dinâmica, projetada para proporcionar uma visão clara e detalhada das suas finanças. Através de gráficos interativos e personalizáveis, você pode monitorar o desempenho das suas contas, acompanhar transações, analisar investimentos e muito mais. Cada gráfico foi desenvolvido com tecnologia de ponta para garantir precisão e facilidade de interpretação, ajudando você a tomar decisões informadas.',
             skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub'],
-            images: ['../Styles/Assets/dashboardWhiteTheme.png', '../Styles/Assets/dashboardWhiteTheme.png'],
+            images: ['../Styles/Assets/FirstDashboard.png', '../Styles/Assets/SecondDashboard.png'],
             link: 'https://joao8675.github.io/Dashboard/'
         },
         details3: {
             title: 'Login Page',
-            info: 'Este é um painel de controle para um banco.Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsa repellendus voluptates. Odio odit praesentium similique officiis molestiae veniam laboriosam quidem nesciunt, et dolorem ea, aperiam, maiores saepe vitae fuga.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus impedit dolorem amet, fuga officia, numquam exercitationem dignissimos quibusdam sit consequuntur facilis porro eligendi voluptatibus quo provident, laborum deleniti velit incidunt!',
+            info: 'Nossa página de login foi projetada com o usuário em mente, garantindo que o acesso à sua conta seja feito de maneira simples e eficiente. Ao acessar a página, você encontrará um layout limpo e organizado, com campos claramente identificados para inserir seu endereço de e-mail e senha. A interface é responsiva, adaptando-se perfeitamente a qualquer dispositivo, seja ele um desktop, tablet ou smartphone, para que você possa acessar sua conta de qualquer lugar.',
             skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub'],
-            images: ['../Styles/Assets/Delicacy.png', '../Styles/Assets/Delicacy.png'],
+            images: ['../Styles/Assets/LoginPagefirst.png', '../Styles/Assets/LoginPagefirst.png'],
             link: 'https://joao8675.github.io/Login-Page/'
         },
         details4: {
             title: 'To-Do List',
-            info: 'Este é um painel de controle para um banco.Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsa repellendus voluptates. Odio odit praesentium similique officiis molestiae veniam laboriosam quidem nesciunt, et dolorem ea, aperiam, maiores saepe vitae fuga.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus impedit dolorem amet, fuga officia, numquam exercitationem dignissimos quibusdam sit consequuntur facilis porro eligendi voluptatibus quo provident, laborum deleniti velit incidunt!',
+            info: 'Nossa To-Do List foi criada utilizando a poderosa biblioteca React, que garante uma experiência de usuário rápida e responsiva. Combinamos isso com o Tailwind CSS para oferecer uma interface moderna e estilizada, que é ao mesmo tempo funcional e agradável aos olhos.',
             skills: ['HTML:5', 'React', 'Tailwind', 'JavaScript', 'Git', 'GitHub'],
-            images: ['../Styles/Assets/dashboardWhiteTheme.png', '../Styles/Assets/dashboardWhiteTheme.png'],
+            images: ['../Styles/Assets/To-do-List.png', '../Styles/Assets/To-do-List.png'],
             link: 'https://joao8675.github.io/To-do-list/'
         },
         details5: {
             title: 'Portifolio v1',
-            info: 'Este é um painel de controle para um banco.Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsa repellendus voluptates. Odio odit praesentium similique officiis molestiae veniam laboriosam quidem nesciunt, et dolorem ea, aperiam, maiores saepe vitae fuga.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus impedit dolorem amet, fuga officia, numquam exercitationem dignissimos quibusdam sit consequuntur facilis porro eligendi voluptatibus quo provident, laborum deleniti velit incidunt!',
+            info: 'sta versão do meu portfólio foi desenvolvida com uma combinação de tecnologias modernas para garantir uma experiência de usuário rápida, responsiva e visualmente atraente. Utilizei HTML, CSS e JavaScript para estruturar e estilizar o site Os projetos estão destacados em uma seção dedicada, onde cada um é apresentado com uma descrição detalhada, tecnologias utilizadas e links para o repositório no GitHub e para a versão ao vivo, quando aplicável.',
             skills: ['HTML', 'CSS', 'JavaScript', 'Git', 'GitHub'],
-            images: ['../Styles/Assets/Delicacy.png', '../Styles/Assets/Delicacy.png'],
+            images: ['../Styles/Assets/PortfolioFirst.png', '../Styles/Assets/PortfolioSecond.png'],
             link: 'https://joao8675.github.io/Portifolio/'
         },
     };
